31d7935d0cf6eff329df2cbdfb2f8880d3a3545e
AldwinAkm <jeryaklamavo@gmail.com>
2025-02-26 20:19:54 +0100

    Creation index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Accueil</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container mt-5 text-center">
+        <h1>Bienvenue sur notre système de réservation</h1>
+        <a href="register.php" class="btn btn-primary">S'inscrire</a>
+        <a href="login.php" class="btn btn-success">Se connecter</a>
+    </div>
+</body>
+</html>

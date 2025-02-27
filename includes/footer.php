e12a035aa39d31f2b4ce9d4c18341ed48f8386a5
AldwinAkm <jeryaklamavo@gmail.com>
2025-02-27 16:30:53 +0100

    Create footer.php

---
+++ includes/footer.php
@@ -0,0 +1,17 @@
+<footer class="bg-dark text-white text-center p-3">
+    <div class="container text-center">
+        <p>&copy; Système de réservation. Tous droits réservés.</p>
+        <nav class="mt-2">
+            <a href="#" class="text-white mx-2">Conditions d'utilisation</a>
+            <a href="#" class="text-white mx-2">Politique de confidentialité</a>
+        </nav>
+    </div>    
+</footer>  
+</body>
+</html>
+
+
+
+
+
+

7570fc1eb408b8d2c095371e8ad92d68866d8979
AldwinAkm <jeryaklamavo@gmail.com>
2025-02-27 16:33:31 +0100

    Include header and footer

--- index.php
+++ index.php
@@ -1,15 +1,9 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <title>Accueil</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
-</head>
-<body>
+<?php include 'includes/header.php'; ?>
+    <main>
     <div class="container mt-5 text-center">
         <h1>Bienvenue sur notre système de réservation</h1>
         <a href="register.php" class="btn btn-primary">S'inscrire</a>
         <a href="login.php" class="btn btn-success">Se connecter</a>
     </div>
-</body>
-</html>
+    </main>
+<?php include 'includes/footer.php'; ?>    

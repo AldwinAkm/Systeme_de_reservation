b73b3da32f0198943a4760a9843b9619997d811d
AldwinAkm <jeryaklamavo@gmail.com>
2025-02-27 23:03:07 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,32 @@
 <?php include 'includes/header.php'; ?>
     <main>
+    <section class="py-5">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-6">
+                    <h1>Bienvenue sur QuickResa, notre plateforme de réservation en ligne !</h1>
+                    <p>Simplifiez la gestion de vos rendez-vous avec notre système de réservation en ligne rapide et sécurisé. <br>
+                    
+ En quelques clics, vous pouvez : <br>
+
+✅ Créer un compte et gérer vos informations personnelles. <br>
+✅ Consulter les disponibilités grâce à un calendrier interactif. <br>
+✅ Réserver un créneau horaire en toute simplicité. <br>
+✅ Annuler ou modifier un rendez-vous si besoin. <br>
+
+Notre plateforme vous garantit une expérience fluide et sécurisée, avec un accès 24h/24 et 7j/7 pour gérer vos réservations à tout moment. <br>
+
+💡 Inscrivez-vous dès maintenant et réservez votre créneau en toute tranquillité !</p>
+<a href="register.php" class="btn btn-primary">Commencer dès maintenant</a>
+                </div>
+                <div class="col-lg-6">
+                    <img src="assets/img/reserv.webp" alt=""  style="height: 400px;">
+                </div>
+            </div>
+        </div>
+    </section>
     <div class="container mt-5 text-center">
-        <h1>Bienvenue sur notre système de réservation</h1>
-        <a href="register.php" class="btn btn-primary">S'inscrire</a>
-        <a href="login.php" class="btn btn-success">Se connecter</a>
+        
     </div>
     </main>
 <?php include 'includes/footer.php'; ?>    

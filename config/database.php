fa29945b0cf518fe4ee9ca68e9a0f4109931a6be
AldwinAkm <jeryaklamavo@gmail.com>
2025-02-27 23:02:47 +0100

    Create database.php

---
+++ config/database.php
@@ -0,0 +1,15 @@
+<?php
+// config/database.php
+
+$host = 'localhost';  // ou l'adresse de votre serveur MySQL
+$db = 'reservations'; // Nom de la base de données (vous pouvez le changer)
+$user = 'root';       // Nom d'utilisateur MySQL
+$pass = '';           // Mot de passe MySQL
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erreur de connexion à la base de données : " . $e->getMessage());
+}
+?>
